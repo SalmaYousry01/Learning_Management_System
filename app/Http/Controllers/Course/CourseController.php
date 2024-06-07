16fee598ecdd30b6f9638f76fba9fa0fdf49346f
SalmaYousry01 <syamh26701@gmail.com>
2024-06-08 02:49:40 +0300

    feat: Implement basic LMS with user registration, CRUD operations for course management, and contact form

---
+++ app/Http/Controllers/Course/CourseController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Course;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $courses = Course::all();
+        return view('courses.index', ['courses' => $courses]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('courses.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //separate request
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+
+        Course::create($request->all());
+        return redirect()->route('courses.index')->with('success', 'Course created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Course $course)
+    {
+        return view('courses.show', ['course' => $course]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Course $course)
+    {
+        return view('courses.edit', ['course' => $course]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Course $course)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+
+        $course->update($request->all());
+        return redirect()->route('courses.index')->with('success', 'Course updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Course $course)
+    {
+        $course->delete();
+        return redirect()->route('courses.index')->with('success', 'Course deleted successfully.');
+    }
+}
