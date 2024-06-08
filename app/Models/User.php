0cc3e33993c319976d01eee7eea343c916403e85
SalmaYousry01 <syamh26701@gmail.com>
2024-06-08 22:18:49 +0300

    docs:Add README with installation instructions

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    // public function courses()
+    // {
+    //     return $this->hasMany(Course::class);
+    // }
+
+    
 }
