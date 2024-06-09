d21084f42ae83769b6583d25f2e2e58a203c3b62
SalmaYousry01 <syamh26701@gmail.com>
2024-06-09 11:59:53 +0300

    style:Add link to redirect register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,9 +39,15 @@
                 </a>
             @endif
 
+
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+        </div>
+        <div>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Don\'t have account?') }}
+                </a>
         </div>
     </form>
 </x-guest-layout>
